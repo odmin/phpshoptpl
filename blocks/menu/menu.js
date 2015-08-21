6113e0be7aa232db5780aee77c123456a7892b9e
odmin <linux_tosvald@bk.ru>
2015-08-21 16:18:14 +0300

    menu js

--- blocks/menu/menu.js
+++ blocks/menu/menu.js
@@ -1,6 +1,17 @@
 jQuery(document).ready(function($) {
-    $('.menu-cat__item').hover(
-        function () {$(this).find('.menu-cat-child').css('display', 'inline-block');},
-        function () {$(this).find('.menu-cat-child').css('display', 'none')}
-      )
+	if (window.matchMedia("(min-width: 1023px)").matches) {
+	    $('.menu-cat__item').hover(
+	        function () {$(this).find('.menu-cat-child').css('display', 'inline-block');},
+	        function () {$(this).find('.menu-cat-child').css('display', 'none')}
+	      )
+	}
+	else{
+		$('.menu-cat__item').on('click', 
+			function () {
+				$('.menu-cat-child').each(function () {
+					$(this).css('display', 'none')
+				});
+				$(this).find('.menu-cat-child').css('display', 'inline-block');
+			})
+	}
   })
--- javascript/production.js
+++ javascript/production.js
@@ -5,8 +5,19 @@ $(document).ready(function(){
 	pause:5000
 	});
 });;jQuery(document).ready(function($) {
-    $('.menu-cat__item').hover(
-        function () {$(this).find('.menu-cat-child').css('display', 'inline-block');},
-        function () {$(this).find('.menu-cat-child').css('display', 'none')}
-      )
+	if (window.matchMedia("(min-width: 1023px)").matches) {
+	    $('.menu-cat__item').hover(
+	        function () {$(this).find('.menu-cat-child').css('display', 'inline-block');},
+	        function () {$(this).find('.menu-cat-child').css('display', 'none')}
+	      )
+	}
+	else{
+		$('.menu-cat__item').on('click', 
+			function () {
+				$('.menu-cat-child').each(function () {
+					$(this).css('display', 'none')
+				});
+				$(this).find('.menu-cat-child').css('display', 'inline-block');
+			})
+	}
   })
